KarbitsCode/nativefier

commit 27ecfcbeca9c764c119525695f67ec10a8ab5c8a

Check for improperly-formatted arguments (fix #885) (PR #1080)
Verifies short arguments do not contain an extra dash ( right: `-h`, error: `--h` )
Verifies long arguments contain at least two dashes ( right: `--help`, error: `-help` )

src/cli.ts
@@ -6,6 +6,7 @@ import * as dns from 'dns';
 import * as log from 'loglevel';
 
 import { buildNativefierApp } from './main';
+import { isArgFormatValid } from './helpers/helpers';
 import { isWindows } from './helpers/helpers';
 
 // package.json is `require`d to let tsc strip the `src` folder by determining
@@ -68,6 +69,12 @@ function checkInternet(): void {
 if (require.main === module) {
   const sanitizedArgs = [];
   process.argv.forEach((arg) => {
+    if (isArgFormatValid(arg) === false) {
+      log.error(
+        `Invalid argument passed: ${arg} .\nNativefier supports short options (like "-n") and long options (like "--name"), all lowercase. Run "nativefier --help" for help.\nAborting`,
+      );
+      process.exit(1);
+    }
     if (sanitizedArgs.length > 0) {
       const previousArg = sanitizedArgs[sanitizedArgs.length - 1];
 

src/helpers/helpers.ts
@@ -139,3 +139,10 @@ export function getAllowedIconFormats(platform: string): string[] {
   log.debug(`Allowed icon formats when building for ${platform}:`, formats);
   return formats;
 }
+
+export function isArgFormatValid(arg: string): boolean {
+  return (
+    /^-[a-z]$/i.exec(arg) !== null ||
+    /^--[a-z]{2,}(-[a-z]{2,})*$/i.exec(arg) !== null
+  );
+}

src/helpers/helpers.test.ts
@@ -1,0 +1,39 @@
+import { isArgFormatValid } from './helpers';
+
+describe('isArgFormatValid', () => {
+  test('is true for short arguments', () => {
+    expect(isArgFormatValid('-t')).toBe(true);
+  });
+
+  test('is false for improperly formatted short arguments', () => {
+    expect(isArgFormatValid('--t')).toBe(false);
+  });
+
+  test('is true for long arguments', () => {
+    expect(isArgFormatValid('--test')).toBe(true);
+  });
+
+  test('is false for improperly formatted long arguments', () => {
+    expect(isArgFormatValid('---test')).toBe(false);
+  });
+
+  test('is false for improperly formatted long arguments', () => {
+    expect(isArgFormatValid('-test')).toBe(false);
+  });
+
+  test('is true for long arguments with dashes', () => {
+    expect(isArgFormatValid('--test-run')).toBe(true);
+  });
+
+  test('is false for improperly formatted long arguments with dashes', () => {
+    expect(isArgFormatValid('--test--run')).toBe(false);
+  });
+
+  test('is true for long arguments with many dashes', () => {
+    expect(isArgFormatValid('--test-run-with-many-dashes')).toBe(true);
+  });
+
+  test('is false for improperly formatted excessively long arguments', () => {
+    expect(isArgFormatValid('--test--run--with--many--dashes')).toBe(false);
+  });
+});

commit 2f28b3731d45a123c892faaca3fa845f379dda5b

Fix arg validation regression in #1080 (fix #1083)

src/cli.ts
@@ -6,7 +6,7 @@ import * as dns from 'dns';
 import * as log from 'loglevel';
 
 import { buildNativefierApp } from './main';
-import { isArgFormatValid } from './helpers/helpers';
+import { isArgFormatInvalid } from './helpers/helpers';
 import { isWindows } from './helpers/helpers';
 
 // package.json is `require`d to let tsc strip the `src` folder by determining
@@ -69,7 +69,7 @@ function checkInternet(): void {
 if (require.main === module) {
   const sanitizedArgs = [];
   process.argv.forEach((arg) => {
-    if (isArgFormatValid(arg) === false) {
+    if (isArgFormatInvalid(arg)) {
       log.error(
         `Invalid argument passed: ${arg} .\nNativefier supports short options (like "-n") and long options (like "--name"), all lowercase. Run "nativefier --help" for help.\nAborting`,
       );

src/helpers/helpers.ts
@@ -140,9 +140,13 @@ export function getAllowedIconFormats(platform: string): string[] {
   return formats;
 }
 
-export function isArgFormatValid(arg: string): boolean {
+/**
+ * Refuse args like '--n' or '-name', we accept either short '-n' or long '--name'
+ */
+export function isArgFormatInvalid(arg: string): boolean {
   return (
-    /^-[a-z]$/i.exec(arg) !== null ||
-    /^--[a-z]{2,}(-[a-z]{2,})*$/i.exec(arg) !== null
+    arg.startsWith('---') ||
+    /^--[a-z]$/i.exec(arg) !== null ||
+    /^-[a-z]{2,}$/i.exec(arg) !== null
   );
 }

src/helpers/helpers.test.ts
@@ -1,39 +1,31 @@
-import { isArgFormatValid } from './helpers';
+import { isArgFormatInvalid } from './helpers';
 
-describe('isArgFormatValid', () => {
-  test('is true for short arguments', () => {
-    expect(isArgFormatValid('-t')).toBe(true);
+describe('isArgFormatInvalid', () => {
+  test('is false for correct short args', () => {
+    expect(isArgFormatInvalid('-t')).toBe(false);
   });
 
-  test('is false for improperly formatted short arguments', () => {
-    expect(isArgFormatValid('--t')).toBe(false);
+  test('is true for improperly double-dashed short args', () => {
+    expect(isArgFormatInvalid('--t')).toBe(true);
   });
 
-  test('is true for long arguments', () => {
-    expect(isArgFormatValid('--test')).toBe(true);
+  test('is false for correct long args', () => {
+    expect(isArgFormatInvalid('--test')).toBe(false);
   });
 
-  test('is false for improperly formatted long arguments', () => {
-    expect(isArgFormatValid('---test')).toBe(false);
+  test('is true for improperly triple-dashed long args', () => {
+    expect(isArgFormatInvalid('---test')).toBe(true);
   });
 
-  test('is false for improperly formatted long arguments', () => {
-    expect(isArgFormatValid('-test')).toBe(false);
+  test('is true for improperly single-dashed long args', () => {
+    expect(isArgFormatInvalid('-test')).toBe(true);
   });
 
-  test('is true for long arguments with dashes', () => {
-    expect(isArgFormatValid('--test-run')).toBe(true);
+  test('is false for correct long args with dashes', () => {
+    expect(isArgFormatInvalid('--test-run')).toBe(false);
   });
 
-  test('is false for improperly formatted long arguments with dashes', () => {
-    expect(isArgFormatValid('--test--run')).toBe(false);
-  });
-
-  test('is true for long arguments with many dashes', () => {
-    expect(isArgFormatValid('--test-run-with-many-dashes')).toBe(true);
-  });
-
-  test('is false for improperly formatted excessively long arguments', () => {
-    expect(isArgFormatValid('--test--run--with--many--dashes')).toBe(false);
+  test('is false for correct long args with many dashes', () => {
+    expect(isArgFormatInvalid('--test-run-with-many-dashes')).toBe(false);
   });
 });